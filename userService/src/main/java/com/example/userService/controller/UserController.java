778d425411c462ece9cb536215c2f1043feb6cb4
chaithramysr <chaithrab.mysr@gmail.com>
2024-08-02 17:47:44 +0530

    initial userservice api's

---
+++ userService/src/main/java/com/example/userService/controller/UserController.java
@@ -0,0 +1,42 @@
+package com.example.userService.controller;
+
+import com.example.userService.entity.User;
+import com.example.userService.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/addUser")
+    public String addUser(@RequestBody User user){
+        return userService.addUser(user);
+    }
+
+    @GetMapping("/getAllUsers")
+    public List<User> getAllUsers(){
+        return userService.getAllUsers();
+    }
+
+    @GetMapping("/getUserById/{id}")
+    public User getUser(@PathVariable("id") String userId){
+        return userService.getUser(userId);
+    }
+
+    @PutMapping("/updateUser")
+    public String updateUser(@RequestBody User user){
+        return userService.updateUser(user);
+    }
+
+    @DeleteMapping("/deleteUser/{id}")
+    public String deleteUser(@PathVariable("id") String userId){
+        return userService.deleteUser(userId);
+    }
+}
